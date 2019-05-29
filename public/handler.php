15dfd3a23b45bd131aa343f9136c25d33464e673
huorehu <huorehu@gmail.com>
2019-05-29 23:19:30 +0300

    add routes handler

---
+++ public/handler.php
@@ -0,0 +1,27 @@
+<?php
+
+spl_autoload_register(function($className) {
+    require dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+});
+define('RESOURCES_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR);
+
+use app\Validator;
+
+$validator = new Validator();
+
+switch ($_GET['form']) {
+    case 'image-generator':
+        $imageWidth = $validator->validate($_GET['width']);
+        $imageHeight = $validator->validate($_GET['height']);
+        $circleRadius = $validator->validate($_GET['radius']);
+        $amountCircles = $validator->validate($_GET['amount']);
+        require RESOURCES_PATH . 'handlers' . DIRECTORY_SEPARATOR . 'imagePaintHandler.php';
+        break;
+    case 'slider':
+        require 'slider.php';
+        break;
+    default:
+        http_response_code(404);
+}
+
+

6273b507708d249e2bf386da5b7b6d89c5eaa2b0
huorehu <huorehu@gmail.com>
2019-05-29 23:59:14 +0300

    add handler for images painter

---
+++ resources/handlers/imagePaintHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+use app\CirclesGenerator;
+use app\ImageWriter;
+
+if ($imageWidth === -1 || $imageHeight === -1 || $circleRadius === -1 || $amountCircles === -1
+    || !$validator->validateRadius($imageWidth, $imageHeight, $circleRadius)) {
+    http_response_code(500);
+    exit();
+}
+
+$circlesGenerator = new CirclesGenerator();
+$imageWriter = new ImageWriter(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'public'. DIRECTORY_SEPARATOR . 'img' . DIRECTORY_SEPARATOR);
+
+$image = $circlesGenerator->getCirclesImage($imageWidth, $imageHeight, $circleRadius, $amountCircles);
+$imageWriter->writeImage($image);
+
+header('Content-Type: image/png');
+imagepng($image);

56d3d6b5551f183dc0b210e3356dea7120e28f35
huorehu <huorehu@gmail.com>
2019-05-29 23:28:32 +0300

    add slider page

---
+++ public/slider.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Colored circles slider</title>
+  <link rel="stylesheet" href="css/slider.css">
+</head>
+<body>
+<div class="wrapper">
+  <section>
+    <div id="slider" class="slider">
+      <div class="slider-current">
+        <img src="img/5ceee8ea1e2a6.png" alt="0">
+      </div>
+      <ul class="slider-previews"></ul>
+    </div>
+  </section>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="js/slider.js"></script>
+</body>
+</html>
